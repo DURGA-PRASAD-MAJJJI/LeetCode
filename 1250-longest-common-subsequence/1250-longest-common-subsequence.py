4e679ed0a237100243e0f24fe28128771e9d8933
MAJJI DURGA PRASAD <162361023+DURGA-PRASAD-MAJJJI@users.noreply.github.com>
2025-06-24 10:48:32 +0530

    Time: 512 ms (42.5%), Space: 43.5 MB (34.69%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -2,15 +2,13 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n=len(text1)
         m=len(text2)
-        self.dd=[[-1 for _ in range(m+1)]for _ in range(n+1)]
-        return self.solve(text1,text2,n,m)
-    def solve(self,text1,text2,n,m):
-        if n==0 or m==0:
-            return 0
-        if self.dd[n][m]!=-1:
-            return self.dd[n][m]
-        if text1[n-1]==text2[m-1]:
-            self.dd[n][m]=1+self.solve(text1,text2,n-1,m-1)
-        else:
-            self.dd[n][m]=max(self.solve(text1,text2,n-1,m),self.solve(text1,text2,n,m-1))
-        return self.dd[n][m]
+        dd=[[0 for _ in range(m+1)]for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                # if dd[i][j]!=0:
+                #     return dd[i][j]
+                if text1[i-1]==text2[j-1]:
+                    dd[i][j]=1+dd[i-1][j-1]
+                else:
+                    dd[i][j]=max(dd[i-1][j],dd[i][j-1])
+        return dd[n][m]
