f373004fbfcecca1676b5e434b33f1d5398caa9a
MAJJI DURGA PRASAD <162361023+DURGA-PRASAD-MAJJJI@users.noreply.github.com>
2025-06-24 15:17:55 +0530

    Time: 0 ms (100%), Space: 17.7 MB (54.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        a,b=0,0
-        for i in nums:
-            t=max(a,b+i)
-            b=a
-            a=t
-        return a
-
-
+        n=len(nums)
+        self.dd=[-1 for _ in range(n+1)]
+        return self.solve(nums,n-1)
+    def solve(self,nums,i):
+        if i<0:
+            return 0
+        if self.dd[i]!=-1:
+            return self.dd[i]
+        inc=nums[i]+self.solve(nums,i-2)
+        exc=self.solve(nums,i-1)
+        self.dd[i]=max(inc,exc)
+        return self.dd[i]
         
